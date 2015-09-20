e3a39cef47b14e11e9e125215f6ac794de0480a5
Nenad Djordjevic <ndjordjevic@gmail.com>
2015-09-20 11:43:33 +0200

    Initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('dentalrecMeanApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
